a4dcd7a263ecddbe3d6930353af7854637ab23f3
johan <johanna.zagon@gmail.com>
2025-06-01 15:53:07 +0200

    use main

--- train.py
+++ train.py
@@ -97,69 +97,71 @@ def test(data_dir_test: str, batch_size: int, model: models.MobileNetV2, device:
     test_acc = evaluate(test_loader, model)
     print(f"Balaced test accuracy: {test_acc:.4f}")
 
-args = arg_parse()
+def main(data_dir_train: str, output_model_path: str, batch_size: int, num_epochs:int, learning_rate: float):
+    # --- 1. Settings ---
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Using device: {device}")
 
-# --- 1. Settings ---
-data_dir_train = args.data_dir_train  # your data path
-output_model_path = '/content/drive/MyDrive/farmers_eye/outputs/model.pth'
-batch_size = args.batch_size # Previously was 32
-num_epochs = args.num_epochs
-learning_rate = 0.0035148759
-validation_percent = 0.1
-test_percent = 0.1
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"Using device: {device}")
+    # --- 2. Data transforms ---
+    # done now in preprocessing
 
-# --- 2. Data transforms ---
-# done now in preprocessing
-
-# --- 3. Load training dataset ---
-train_dataset = PreprocessedTensorDataset(data_dir_train, to_device=device)
-num_classes = len(train_dataset.classes)
-print(f"Found {len(train_dataset)} images, {num_classes} classes: {train_dataset.classes}")
+    # --- 3. Load training dataset ---
+    train_dataset = PreprocessedTensorDataset(data_dir_train, to_device=device)
+    num_classes = len(train_dataset.classes)
+    print(f"Found {len(train_dataset)} images, {num_classes} classes: {train_dataset.classes}")
 
 
 
 
-# --- 4. Split dataset ---
-#n_total = len(full_dataset)
-#n_test = int(test_percent * n_total)
-#n_validation = int(validation_percent * n_total)
-#n_train = n_total - n_validation - n_test
-#train_dataset, validation_dataset, test_dataset = random_split(full_dataset, [n_train, n_validation, n_test])
+    # --- 4. Split dataset ---
+    #n_total = len(full_dataset)
+    #n_test = int(test_percent * n_total)
+    #n_validation = int(validation_percent * n_total)
+    #n_train = n_total - n_validation - n_test
+    #train_dataset, validation_dataset, test_dataset = random_split(full_dataset, [n_train, n_validation, n_test])
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-#validation_loader = DataLoader(validation_dataset, batch_size=batch_size)
-#test_loader = DataLoader(test_dataset, batch_size=batch_size)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    #validation_loader = DataLoader(validation_dataset, batch_size=batch_size)
+    #test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
-# --- 5. Load pre-trained MobileNetV2 ---
-# same weights as in the paper would be: IMAGENET1K_V1 instead of DEFAULT
-model = models.mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
+    # --- 5. Load pre-trained MobileNetV2 ---
+    # same weights as in the paper would be: IMAGENET1K_V1 instead of DEFAULT
+    model = models.mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
 
-# Freeze all layers
-for param in model.parameters():
-    param.requires_grad = False
-# Replace classifier
-model.classifier[1] = nn.Linear(model.last_channel, num_classes)
-model = model.to(device)
+    # Freeze all layers
+    for param in model.parameters():
+        param.requires_grad = False
+    # Replace classifier
+    model.classifier[1] = nn.Linear(model.last_channel, num_classes)
+    model = model.to(device)
 
-# --- 6. Loss and optimizer ---
-criterion = nn.CrossEntropyLoss()
-#optimizer = optim.Adam(model.classifier[1].parameters(), lr=learning_rate)
-optimizer = optim.SGD(model.classifier[1].parameters(), lr=learning_rate, momentum=0.0) # Used in paper: Gradient descent
-scaler = GradScaler()
+    # --- 6. Loss and optimizer ---
+    criterion = nn.CrossEntropyLoss()
+    #optimizer = optim.Adam(model.classifier[1].parameters(), lr=learning_rate)
+    optimizer = optim.SGD(model.classifier[1].parameters(), lr=learning_rate, momentum=0.0) # Used in paper: Gradient descent
+    scaler = GradScaler()
 
-# --- 7. Training loop ---
-for epoch in range(num_epochs):
-    train(train_loader, model, device, epoch, num_epochs, optimizer, criterion, scaler)
+    # --- 7. Training loop ---
+    for epoch in range(num_epochs):
+        train(train_loader, model, device, epoch, num_epochs, optimizer, criterion, scaler)
 
-train_acc = evaluate(train_loader, model)
-print(f"Training Accuracy={train_acc:.4f}")
+    train_acc = evaluate(train_loader, model)
+    print(f"Training Accuracy={train_acc:.4f}")
 
-# --- 8. Test accuracy ---
-if args.test == True:
-    test(args.data_dir_test, batch_size, model, device)
+    # --- 8. Test accuracy ---
+    if args.test == True:
+        test(args.data_dir_test, batch_size, model, device)
 
-# --- 9. Save model ---
-torch.save(model.state_dict(), output_model_path)
-print(f"Model saved to {output_model_path}")
+    # --- 9. Save model ---
+    torch.save(model.state_dict(), output_model_path)
+    print(f"Model saved to {output_model_path}")
+
+
+if __name__ == "__main__":    
+    args = arg_parse()
+    data_dir_train = args.data_dir_train  # your data path
+    output_model_path = './outputs/model.pth'
+    batch_size = args.batch_size
+    num_epochs = args.num_epochs
+    learning_rate = 0.0035148759
+    main(data_dir_train, output_model_path, batch_size, num_epochs, learning_rate)
